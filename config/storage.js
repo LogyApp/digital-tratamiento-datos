9eb2c39f868072c6c7c1be8db8ab42c33cbc6abe
LogyApp <seguimientologyser@gmail.com>
2026-02-20 10:12:02 -0500

    feat: manage account cloud

--- config/storage.js
+++ config/storage.js
@@ -5,12 +5,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const storage = new Storage({
-    keyFilename: path.join(__dirname, '../gcs-key.json')
-});
+// const storage = new Storage({
+//     keyFilename: path.join(__dirname, '../gcs-key.json')
+// });
+const storage = new Storage();
 
 const bucket = storage.bucket(process.env.GCS_BUCKET_NAME);
 
